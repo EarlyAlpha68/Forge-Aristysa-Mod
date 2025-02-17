213dcca010d6844ed6798308d8835cebf92e9ea5
Pierre LAMOUR <pierrelamournew@hotmail.com>
2025-02-17 22:02:06 +0100

    Commit

--- src/main/java/net/earlyalpha/aristysa/networking/packet/EnderEyeC2SPacket.java
+++ src/main/java/net/earlyalpha/aristysa/networking/packet/EnderEyeC2SPacket.java
@@ -1,11 +1,14 @@
 package net.earlyalpha.aristysa.networking.packet;
 
+import net.earlyalpha.aristysa.networking.ModMessages;
 import net.earlyalpha.aristysa.screen.CyberwareGuiMenuType;
 import net.earlyalpha.aristysa.util.EarlyUtil;
 import net.earlyalpha.aristysa.util.ImplantUsage;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraftforge.network.NetworkEvent;
 
@@ -26,7 +29,18 @@ public class EnderEyeC2SPacket {
         NetworkEvent.Context context = supplier.get();
         context.enqueueWork(() ->{
             ServerPlayer serverPlayer = context.getSender();
-            ImplantUsage.teleportEnderEyePlayer(serverPlayer);
+            CompoundTag nbt = new CompoundTag();
+            nbt.putInt("cyberLegTier", EarlyUtil.getImplantTier(serverPlayer,"cyberLegTier"));
+            nbt.putInt("enderEyeTier",EarlyUtil.getImplantTier(serverPlayer,"enderEyeTier"));
+            nbt.putInt("golemArmTier",EarlyUtil.getImplantTier(serverPlayer,"golemArmTier"));
+            nbt.putInt("opticalCamoTier",EarlyUtil.getImplantTier(serverPlayer,"opticalCamoTier"));
+            nbt.putInt("subdermalArmorTier",EarlyUtil.getImplantTier(serverPlayer,"subdermalArmorTier"));
+            nbt.putInt("wardenHeartTier",EarlyUtil.getImplantTier(serverPlayer,"wardenHeartTier"));
+            ModMessages.sendToPlayer(new Cyberware_SyncS2CPacket(nbt),serverPlayer);
+           // ImplantUsage.teleportEnderEyePlayer(serverPlayer);
+            serverPlayer.openMenu(new SimpleMenuProvider(
+                    (containerId, playerInventory, player) -> new CyberwareGuiMenuType(containerId, playerInventory),
+                    Component.translatable("menu.title.aristysa.title")));
         });
         return true;
     }
